a245c4059f9fb820aacdedf5f5a03dbd0bc437c9
rmenendezdaw <rmenendeziestacio@gmail.com>
2020-10-11 22:15:00 +0200

    Improve slider

--- 5_frontend/src/js/home/homeSlider.component.js
+++ 5_frontend/src/js/home/homeSlider.component.js
@@ -4,8 +4,8 @@ class homeSliderCtrl {
         this.noWrapSlides = false;
 
         this.slides = [ {image:'images/1.jpg',text:"Get funding for your code projects.",id:0},
-                    {image:'https://www.kaliparmley.com/wp-content/uploads/2014/11/Video-Feature-2-1354x400.jpg',text:"Supports other projects to keep the community growing.",id:1},
-                    {image:'https://k44.kn3.net/taringa/4/8/7/9/0/0/6/facundo_520/1B7.jpg?2170',text:"Other users can do the code you don't know how to do.",id:2}];
+                    {image:'images/2.jpg',text:"Supports other projects to keep the community growing.",id:1},
+                    {image:'images/3.jpg',text:"Other users can do the code you don't know how to do.",id:2}];
     }
 }
 
